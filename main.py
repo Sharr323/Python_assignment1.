e359edc7e320957920ddfb1ccb6390554de9f1b8
Ruvarashe MG <sharonava34@gmail.com>
2025-08-21 08:13:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-  # This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ 
 # Constant (written in uppercase)
 TAX_RATE = 0.20  # 20% tax
 
-# Variables
+# Variables (written in lowercase)
 customer_name = "Ruvarashe Magaya"
 item = "Wooden Dining Table"
 price = 350.00  # in USD
@@ -28,7 +13,7 @@ subtotal = price * quantity
 tax = subtotal * TAX_RATE
 total = subtotal + tax
 
-# Output using print function
+# Print function
 print("Customer Name:", customer_name)
 print("Item Purchased:", item)
 print("Quantity:", quantity)
@@ -36,3 +21,4 @@ print("Price per Item: $", price)
 print("Subtotal: $", subtotal)
 print("Tax (15%): $", tax)
 print("Total Amount Due: $", total)
+
